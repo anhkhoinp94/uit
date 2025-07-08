93bca7930c3161fa17cb5446e3b157027a5d6e6c
anhkhoinp94 <anhkhoi.np94@gmail.com>
2025-07-08 16:47:26 +0700

    2025-07-08_0447 PM

---
+++ up.bat
@@ -0,0 +1,8 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+echo %mydate%_%mytime%
+echo %time%
+git add .
+git commit -m "%mydate%_%mytime%"
+git push
